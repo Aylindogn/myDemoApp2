121d14d517976e89832a693d8d6ac86e739efb5c
Aylindogn <aylindogan247@gmail.com>
2024-03-11 22:25:29 +0300

    I have started my awesome projectgit statusgit status It will be great.

--- src/test/java/com/mycompany/app/App.java
+++ src/test/java/com/mycompany/app/App.java
@@ -1,40 +1,24 @@
 package com.mycompany.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class App{
 
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+    public static boolean search(ArrayList<Integer> array, int e) {
+    System.out.println("inside search");
+    if (array == null) return false;
+
+    for (int elt : array) {
+        if (elt == e) return true;
     }
+    return false;
+}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
 
     public void testFound() {
         ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
